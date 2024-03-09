3d96dbaeee267d7b9b83f645fae90970c9796ed5
Yury Odintsov <yury.odinson@gmail.com>
2024-03-09 15:38:21 +0300

    feat: tabs, second title & separator

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,28 @@
+import {TabFirst} from "./TabFirst";
+import {TabSecond} from "./TabSecond";
+
 export const Main = () => {
     return (
         <div className="container">
             <div className="container-title">
                 <h1 className="container-title__item">ХОБЛ: мифы и реальность</h1>
             </div>
+
+            <div className="container-tabs">
+                <TabFirst/>
+                <TabSecond/>
+            </div>
+
+            <div className="container-title">
+                <h1 className="container-title__item">Терапия ХОБЛ: что в фокусе?</h1>
+            </div>
+
+            <div className="container-separator">
+                <h2>Даже 1 среднетяжелое обострение — сигнал к увеличению объема терапии</h2>
+            </div>
+
+            <h3>Приоритетные направления фармакотерапевтической стратегии при ХОБЛ&sup1;:</h3>
+
         </div>
     );
 };
--- src/components/TabFirst.tsx
+++ src/components/TabFirst.tsx
@@ -1,3 +1,27 @@
-export const Tab = () => {
+import icon from "../assets/img/tab-icon.png";
+import buttonIcon from "../assets/img/tab-button.png";
+import buttonOpen from "../assets/img/tab-open.png";
 
-}
+export const TabFirst = () => {
+    return (
+        <div className="tab">
+            <img src={icon} alt="icon" width={80} height={80}/>
+            <div className="tab-answer">
+                <p>
+                    ХОБЛ болеют <span>преимущественно мужчины?</span>
+                </p>
+                <img src={buttonIcon} alt="icon"/>
+            </div>
+
+            <div className="tab-description">
+                <p className="tab-description__item">В десятилетнем исследовании «Генетическая эпидемиология ХОБЛ»
+                    (COPDGene) среди пациентов с ХОБЛ,
+                    диагностированной до 55 лет, было выявлено преобладание...</p>
+                <div className="tab-description__blur"/>
+            </div>
+
+            <img src={buttonOpen} className="tab-open" alt="open tab" width={48} height={48}/>
+
+        </div>
+    );
+};
