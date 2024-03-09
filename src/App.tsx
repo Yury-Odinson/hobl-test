5c164a561ac96ac2a944bc92f10da186b131bcfa
Yury Odintsov <yury.odinson@gmail.com>
2024-03-09 13:41:00 +0300

    feat: header background & container

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/index.scss";
+import figure1 from "../src/assets/img/header-1.png";
+import figure2 from "../src/assets/img/header-2.png";
+import figure3 from "../src/assets/img/header-3.png";
+import {Main} from "./components/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="background">
+                <img className="background__figure3" src={figure3} alt=""/>
+                <img className="background__figure2" src={figure2} alt=""/>
+                <img className="background__figure1" src={figure1} alt=""/>
+            </div>
+            <div className="wrapper">
+                <Main/>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+export const Main = () => {
+    return (
+        <div className="container">
+            <div className="container-title">
+                <h1 className="container-title__item">ХОБЛ: мифы и реальность</h1>
+            </div>
+        </div>
+    );
+};
